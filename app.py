b8e90b9a8bfa74d786c8f8b9a74688b85868f4ca
miguel-pub <miguelebersbach+1@outlook.com>
2025-01-05 13:40:34 +0100

    db actually works

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=False, nullable=False)
+    complete = db.Column(db.Boolean)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("base.html")
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
